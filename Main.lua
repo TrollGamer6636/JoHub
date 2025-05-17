6fc67354fd04a1f86d4ca5ff82563a3336427def
k5d3r <superengel13@gmail.com>
2025-05-17 06:40:54 +0200

    Update KEY_URL to point to the correct Pastebin link for key retrieval.

--- Main.lua
+++ Main.lua
@@ -22,7 +22,7 @@ local player = Players.LocalPlayer
 local isOpen = false
 
 -- Pastebin Raw Key URL (ersetzen mit deinem eigenen Link)
-local KEY_URL = "https://pastebin.com/raw/pukeNBLN"
+local KEY_URL = "https://pastebin.com/raw/UtMFpGke"
 
 -- Bypass-User-Liste
 local BYPASS_USERS = {"k5d2r", "Roblox"} -- Hier beliebig viele Usernamen eintragen
